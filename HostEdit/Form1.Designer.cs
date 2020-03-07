e7036e9b6a3ca14c176e1d03e09fd4bb31f03748
wictor <wictor@hotmail.com.br>
2020-03-07 19:46:15 -0300

    Projeto finalizado.

--- HostEdit/Form1.Designer.cs
+++ HostEdit/Form1.Designer.cs
@@ -28,13 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt = new System.Windows.Forms.TextBox();
+            this.add = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.rm = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(13, 13);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(264, 20);
+            this.txt.TabIndex = 0;
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(283, 13);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 23);
+            this.add.TabIndex = 1;
+            this.add.Text = "Adicionar";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(345, 342);
+            this.listBox1.TabIndex = 2;
+            // 
+            // rm
+            // 
+            this.rm.Location = new System.Drawing.Point(12, 386);
+            this.rm.Name = "rm";
+            this.rm.Size = new System.Drawing.Size(345, 23);
+            this.rm.TabIndex = 3;
+            this.rm.Text = "Remover Selecionados";
+            this.rm.UseVisualStyleBackColor = true;
+            this.rm.Click += new System.EventHandler(this.rm_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(13, 415);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(345, 23);
+            this.save.TabIndex = 4;
+            this.save.Text = "Salvar";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(370, 450);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.rm);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.txt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Edit Host File";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button rm;
+        private System.Windows.Forms.Button save;
     }
 }
 
--- HostEdit/Form1.cs
+++ HostEdit/Form1.cs
@@ -3,17 +3,75 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace HostEdit
 {
     public partial class Form1 : Form
     {
+        private string _path = @"C:\Windows\System32\drivers\etc\hosts";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] readText = File.ReadAllLines(_path);
+            foreach (string s in readText)
+            {
+                if (_isValid(s))
+                    listBox1.Items.Add(_formatString(s));
+            }
+        }
+
+        private object _formatString(string s)
+        {
+            RegexOptions options = RegexOptions.None;
+            Regex regex = new Regex("[ ]{2,}", options);
+            string[] arrayString = regex.Replace(s, " ").Split(' ');
+            if (arrayString.Length <= 1) return arrayString[0];
+            return regex.Replace(s, " ").Split(' ')[1];
+        }
+
+        private bool _isValid(string s)
+        {
+            if (s.Trim().Length <= 0) return false;
+            return s.Trim()[0] != '#';
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            if (txt.Text.Trim().Length <= 0)
+            {
+                MessageBox.Show("Favor entrar com algum valor");
+                return;
+            }
+            listBox1.Items.Add(txt.Text.Trim());
+        }
+
+        private void rm_Click(object sender, EventArgs e)
+        {
+            ListBox.SelectedObjectCollection selectedItems = new ListBox.SelectedObjectCollection(listBox1);
+            selectedItems = listBox1.SelectedItems;
+
+            if (listBox1.SelectedIndex != -1)
+            {
+                for (int i = selectedItems.Count - 1; i >= 0; i--)
+                    listBox1.Items.Remove(selectedItems[i]);
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            string[] newText = listBox1.Items.Cast<string>().ToArray();
+            var newTextFormatted = newText.Select(n => "127.0.0.1 " + n).ToArray();
+            File.WriteAllLines(_path, newTextFormatted);
         }
     }
 }
